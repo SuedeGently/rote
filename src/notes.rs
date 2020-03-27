f183f3bbe03279003dcaa318debce21b6b48baf1
SuedeG <ymp17kwu@uea.ac.uk>
2020-03-27 20:09:11 +0000

    Added convenience feature. + Added command-line argument to vim call to move cursor to EOF.

--- src/notes.rs
+++ src/notes.rs
@@ -59,7 +59,7 @@ pub fn new_note(title: &str) -> Result<(), NotesError> {
     
     add_line(&title)?;
 
-    match Command::new("vim").arg(&target_uri).status() {
+    match Command::new("vim").args(&["+ normal G$", &target_uri]).status() {
         Ok(_x) => return Ok(()),
         Err(_e) => return Err(NotesError::FileNotFound)
     }
